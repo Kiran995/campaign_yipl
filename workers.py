5a414ffa84f29ccb36e52c6d9b8b8277a06882f8
Kiran995 <kir.prz@gmail.com>
2018-08-15 18:23:46 +0545

    new

--- workers.py
+++ workers.py
@@ -2,13 +2,15 @@ import datetime
 from app import db, Campaign, Contact
 import time
 import requests
+from sqlalchemy.sql.expression import and_
 
 while 1:
     current_time = datetime.datetime.strptime('2019-06-12T19:30', "%Y-%m-%dT%H:%M")
     campaigns = db.session.query(Campaign).filter_by(schedule=current_time).all()
     for campaign in campaigns:
         print(campaign.id)
-        campaign_contacts = db.session.query(Contact).filter_by(campaign_id=campaign.id).all()
+        kwargs = {'campaign_id':'campaign.id', 'contact.number_status':'Not Sent'}
+        campaign_contacts = db.session.query(Contact).filter(and_(campaign_id=campaign.id, number_status='Not Sent')).all()
         for campaign_contact in campaign_contacts:
             number = campaign_contact.number
             #Call API here
@@ -18,5 +20,8 @@ while 1:
             test.encoding = 'ISO-8859-1'
             print(test)
             print(test.url)
+            print(test.headers)
+            print(test.json)
+            break
 
     time.sleep(2)
